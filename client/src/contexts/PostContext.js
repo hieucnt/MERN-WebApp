6c7acae9680731dc4be786e19b5e87295eb16f28
hieucnt <maitrunghieu1999hp@gmail.com>
2021-04-07 18:10:37 +0700

    done

--- client/src/contexts/PostContext.js
+++ client/src/contexts/PostContext.js
@@ -1,7 +1,115 @@
-import React from "react";
+import axios from "axios";
+import React, { createContext, useReducer, useState } from "react";
+import { postReducer } from "../reducers/postReducer";
+import {
+  apiUrl,
+  POSTS_LOADED_SUCCESS,
+  POSTS_LOADED_FAIL,
+  ADD_POST,
+  DELETE_POST,
+  UPDATE_POST,
+  FIND_POST,
+} from "./constants";
 
-const PostContext = () => {
-  return <div></div>;
+export const PostContext = createContext();
+
+const PostContextProvider = ({ children }) => {
+  //State
+  const [postState, dispatch] = useReducer(postReducer, {
+    post: null,
+    posts: [],
+    postLoading: true,
+  });
+
+  const [showAddPostModal, setShowAddPostModal] = useState(false);
+  const [showUpdatePostModal, setShowUpdatePostModal] = useState(false);
+
+  const [showToast, setShowToast] = useState({
+    show: true,
+    message: "",
+    type: null,
+  });
+  //Get all posts
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/posts`);
+      if (response.data.success) {
+        dispatch({ type: POSTS_LOADED_SUCCESS, payload: response.data.posts });
+      }
+    } catch (error) {
+      dispatch({ type: POSTS_LOADED_FAIL });
+    }
+  };
+  //Add post
+  const addPost = async (newPost) => {
+    try {
+      const response = await axios.post(`${apiUrl}/posts`, newPost);
+      if (response.data.success) {
+        dispatch({ type: ADD_POST, payload: response.data.post });
+        return response.data;
+      }
+    } catch (error) {
+      return error.response.data
+        ? error.response.data
+        : { success: false, message: "Server error" };
+    }
+  };
+
+  //Delete Post
+  const deletePost = async (postId) => {
+    try {
+      const response = await axios.delete(`${apiUrl}/posts/${postId}`);
+      if (response.data.success) {
+        dispatch({ type: DELETE_POST, payload: postId });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //Find post when user updating post
+  const findPost = (postId) => {
+    const post = postState.posts.find((post) => post._id === postId);
+    dispatch({ type: FIND_POST, payload: post });
+  };
+
+  //Update Post
+  const updatePost = async (updatedPost) => {
+    try {
+      const response = await axios.put(
+        `${apiUrl}/posts/${updatedPost._id}`,
+        updatedPost
+      );
+      if (response.data.success) {
+        dispatch({ type: UPDATE_POST, payload: response.data.post });
+        return response.data;
+      }
+    } catch (error) {
+      return error.response.data
+        ? error.response.data
+        : { success: false, message: "Server error" };
+    }
+  };
+
+  //Post context data
+  const postContextData = {
+    postState,
+    getPosts,
+    showAddPostModal,
+    setShowAddPostModal,
+    addPost,
+    showUpdatePostModal,
+    setShowUpdatePostModal,
+    showToast,
+    setShowToast,
+    deletePost,
+    findPost,
+    updatePost,
+  };
+  return (
+    <PostContext.Provider value={postContextData}>
+      {children}
+    </PostContext.Provider>
+  );
 };
 
-export default PostContext;
+export default PostContextProvider;

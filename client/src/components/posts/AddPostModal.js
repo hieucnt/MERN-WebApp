126305b3d4f785716c439571bc52e7088d4a1391
hieucnt <maitrunghieu1999hp@gmail.com>
2021-04-07 22:21:52 +0700

    Fix Netlify Routing

--- client/src/components/posts/AddPostModal.js
+++ client/src/components/posts/AddPostModal.js
@@ -39,7 +39,7 @@ const AddPostModal = () => {
   };
 
   return (
-    <Modal show={showAddPostModal} animation={false} onHide={resetAddPostData}>
+    <Modal show={showAddPostModal} onHide={resetAddPostData}>
       <Modal.Header closeButton>
         <Modal.Title>What do you want to learn ?</Modal.Title>
       </Modal.Header>
